d115da035b6a84adacefce121e9f2ddcb53bcb99
Alfonso Serrano Galán <asergal@alu.upo.es>
2026-04-29 18:47:11 +0200

    feat: relaciones Eloquent entre modelos

--- laravel/app/Models/Cart.php
+++ laravel/app/Models/Cart.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
