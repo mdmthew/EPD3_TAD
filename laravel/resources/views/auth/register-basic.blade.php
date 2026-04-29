f81ee582f61409c3776b9f8c5f89b0d578adb4a8
AOcchietti <aarzocc@alu.upo.es>
2026-04-29 21:01:37 +0200

    Configuración inicial vistas login y registro con Fortify (Comprobación)

---
+++ laravel/resources/views/auth/register-basic.blade.php
@@ -0,0 +1,12 @@
+<h1>Register</h1>
+
+<form method="POST" action="/register">
+    @csrf
+
+    <input type="text" name="name" placeholder="Nombre" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <input type="password" name="password_confirmation" placeholder="Confirm Password" required>
+
+    <button type="submit">Register</button>
+</form>
